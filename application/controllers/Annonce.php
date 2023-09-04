4f892646499c1da0886eea79659c4cfaaaccec87
Akina1507 <akina15072003@gmail.com>
2023-09-04 09:30:50 +0200

    Is connected sur btn annonce

--- application/controllers/Annonce.php
+++ application/controllers/Annonce.php
@@ -10,7 +10,8 @@ class Annonce extends CI_Controller
     /* ------------------------- */
     public function announce()
     {
-
+        if (isConnected() == false) {
+            redirect("Users/login");
         $this->form_validation->set_rules('nom_animal', 'Nom animal', 'trim|required');
         $this->form_validation->set_rules('puce_animal', 'Puce animal', 'trim|required');
         $this->form_validation->set_rules('img_animal', 'Image animal', 'trim|required|in_list[18-25ans,25-35ans,35-50ans,50-75ans,75-99ans,99-ou-plus]');
@@ -58,5 +59,11 @@ class Annonce extends CI_Controller
             );
         }
         $this->load->view('espace_assos/announce');
+    }
+}
+    public function success()
+    {
+        /* header('refresh:10;url=' . base_url('Users')); */
+        $this->load->view('form/success');
     }
 }
