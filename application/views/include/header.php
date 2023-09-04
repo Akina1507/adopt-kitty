570d0366a7616859e515fe71fc888961683eccb4
Akina1507 <akina15072003@gmail.com>
2023-09-04 09:21:45 +0200

    fichier css renommé

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="/adopt-kitty/assets/css/customms.css">
+    <link rel="stylesheet" href="/adopt-kitty/assets/css/style.css">
     <title>Adopt'Kitty</title>
 </head>
 
@@ -31,12 +31,7 @@
 
 
 	<?php if (isConnected() == false) { ?>
-		<ul class="nav nav-tabs">
-  <li class="nav-item">
-  <a class="nav-link" href="<?php echo base_url("Users") ?>">Accueil</a>
-  </li>
-  </ul>
-	<?php } else { ?>
+	
 		<ul class="nav nav-tabs">
   <li class="nav-item">
   <a class="nav-link" href="<?php echo base_url("Users") ?>">Accueil</a>
