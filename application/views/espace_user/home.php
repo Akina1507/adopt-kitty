2e773bf32dc5e60f9bc63a2bd91f00bf2f4e8e7a
Akina1507 <akina15072003@gmail.com>
2023-09-05 11:53:56 +0200

    design btn assos/user

--- application/views/espace_user/home.php
+++ application/views/espace_user/home.php
@@ -6,23 +6,22 @@ include(APPPATH . "views/include/header.php")
      Vue home bouton + lien 
     --------------------------->
 <div class="row">
-	<img class="background" src="assets/img/fond" alt="">
 	<div class="bottom-0 start-0  padding-10-20 d-flex flex-column justify-content-center align-items-center">
 	<h4 class="p-5 text-center">Je suis..</h4>
-	<div class="d-flex p-3 pl-3 pr-5 text-center justify-content-center bordered">
-			
-	<a class="btn custom-btn" href="<?php
+	<div class="d-flex p-2 text-center justify-content-center bordered">
+		<div class="mx-5">
+			<a class="btn btn-outline-dark m-1" href="<?php
 											echo base_url("Users/login") ?>">Utilisateurs
-		<img src="/adopt-kitty/assets/img/icons_users" alt="">
-		
-	</a>
-	<br>
-	<a class="btn custom-btn" href="<?php
-											echo base_url("Users/login_assos") ?>">Associations
-		<img src="/adopt-kitty/assets/img/icons_assos" alt="">
-	</a>
-			
+				<img src="/adopt-kitty/assets/img/icons_users" alt="">
+			</a>
 		</div>
+		<div class="mx-5">
+			<a class="btn btn-outline-dark m-1" href="<?php
+											echo base_url("Users/login_assos") ?>">Associations
+				<img src="/adopt-kitty/assets/img/icons_assos" alt="">
+			</a>
+		</div>	
+	</div>
 	</div>
 </div>
 
