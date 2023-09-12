c8da7e5b87049277249d27d704159865ee89537c
Marlon2025c <bastien.lecour2025@gmail.com>
2023-09-12 14:45:48 +0200

    🐛

--- application/helpers/session_helper.php
+++ application/helpers/session_helper.php
@@ -3,7 +3,7 @@
 function isConnected()
 {
     $CI = get_instance();
-    if ($CI->session->userdata('id')) {
+    if ($CI->session->userdata('id') || $CI->session->assos('id')) {
         return true;
     } else return false;
 }
