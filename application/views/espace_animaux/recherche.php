53c3d70364256c28a9d2b500f8310346753b6b0b
Akina1507 <akina15072003@gmail.com>
2023-09-19 23:01:40 +0200

    ✅ Continuer page recherche URGENT ! Img chat = annonce assos ! + fiche avec toutes les infos !

--- application/views/espace_animaux/recherche.php
+++ application/views/espace_animaux/recherche.php
@@ -26,33 +26,77 @@ include(APPPATH . "views/include/header.php")
 </div>
 </head>
 <div class="col justify-content-center">
-    <div class="image-container d-flex justify-content-center flex-wrap ">
-        <a class="btn btn-outline-dark m-1" href="<?php
-                                                    echo base_url("Adopt/adoption") ?>">Rencontrer cet animal
-        </a>
-        <div>
-            <?php
-            $images = array(
-                'image2.jpg', 'image8.jpg', 'image11.jpg', 'mikado.jpg',
-                'image5.jpg', 'image6.jpg', 'image7.jpg', 'image1.jpg',
-                'pattenrond.jpg', 'image10.jpg', 'image3.jpg', 'image12.jpg'
-            );
-
-            $numColumns = 4;
-            foreach (array_chunk($images, $numColumns) as $row) {
-                echo '<div class="row">';
-                foreach ($row as $image) {
-                    echo '<div class="col-md-3">';
-                    echo '<img src="' . base_url('/assets/img/' . $image) . '" alt="image">';
-                    echo '</div>';
-                }
-                echo '</div>';
-            }
-            ?>
+    <a class="btn btn-outline-dark m-1" href="<?php
+                                                echo base_url("Adopt/adoption") ?>">Rencontrer cet animal
+    </a>
+    <div class="image-container d-flex justify-content-center">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card-group"> <!-- Avoir que 4 images de chats par lignes -->
+                    <div class="card">
+                        <img class="col-3 card-img-top" src="/adopt-kitty/assets/img/mikado.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Mikado</h5>
+                            <p class="card-text">Male - Siamois</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img class="col-3 card-img-top" src="/adopt-kitty/assets/img/pattenrond.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Pattenrond</h5>
+                            <p class="card-text">Male - Europeen</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img class="col-3 card-img-top" src="/adopt-kitty/assets/img/image2.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Capucine</h5>
+                            <p class="card-text">Femelle - Sacré de birmanie</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img class="col-3 card-img-top" src="/adopt-kitty/assets/img/image11.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Spirou</h5>
+                            <p class="card-text">Male - Croisé</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img class="col-3 card-img-top" src="/adopt-kitty/assets/img/image8.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Tango</h5>
+                            <p class="card-text">Male - Croisé</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img class=" col-3 card-img-top" src="/adopt-kitty/assets/img/image10.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Ultia</h5>
+                            <p class="card-text">Femelle - Europeen</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
 
-<?php
-include(APPPATH . "views/include/footer.php")
-?>
+        <?php
+        /* $images = array(
+                    'image2.jpg', 'image8.jpg', 'image11.jpg', 'mikado.jpg',
+                    'image5.jpg', 'image6.jpg', 'image7.jpg', 'image1.jpg',
+                    'pattenrond.jpg', 'image10.jpg', 'image3.jpg', 'image12.jpg'
+                );
+
+                $numColumns = 4;
+                foreach (array_chunk($images, $numColumns) as $row) {
+                    echo '<div class="row">';
+                    foreach ($row as $image) {
+                        echo '<div class="col-md-3">';
+                        echo '<img src="' . base_url('/assets/img/' . $image) . '" alt="image">';
+                        echo '</div>';
+                    }
+                    echo '</div>';
+                } */
+        ?>
+        <?php
+        include(APPPATH . "views/include/footer.php")
+        ?>
