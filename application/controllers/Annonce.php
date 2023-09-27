cf7784830d8a45928b71b451e74b129def041009
Akina1507 <akina15072003@gmail.com>
2023-09-27 15:49:02 +0200

    📝 commentaire Faire les videos presentation

--- application/controllers/Annonce.php
+++ application/controllers/Annonce.php
@@ -3,22 +3,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Annonce extends CI_Controller
 {
-
-
-    /* ------------------------- */
-    /* Validation formulaire */
-    /* ------------------------- */
     public function annonce()
     {
         if (isConnected() == false) {
             redirect("Users/accueil");
         } else {
-            // on stock les données et on appelle get_races du Chat_Model
             $data['races'] = $this->Chat_Model->get_races();
-
-            // boucle pour parcourir $data['races']
             foreach ($data['races'] as $race) {
-                // on collecter les valeurs de race
                 $race_values[] = $race['value'];
             }
             $this->form_validation->set_rules('nom_animal', 'Nom animal', 'trim|required');
@@ -29,29 +20,17 @@ class Annonce extends CI_Controller
             $this->form_validation->set_rules('sexe_animal', 'Sexe', 'trim|required|in_list[male,femelle]');
             $this->form_validation->set_rules('lieu_animal', 'Lieu', 'trim|required');
             $this->form_validation->set_rules('image_chat', 'Image du chat');
-            // verifie si chiens_radio est vide
             if (!empty($_POST['chiens_radio'])) {
-                $this->form_validation->set_rules('chiens_radio', 'Chiens', 'trim|required|in_list[chiens]');
-            }
+                $this->form_validation->set_rules('chiens_radio', 'Chiens', 'trim|required|in_list[chiens]');}
             if (!empty($_POST['chats_radio'])) {
-                $this->form_validation->set_rules('chats_radio', 'Chats', 'trim|required|in_list[chats]');
-            }
+                $this->form_validation->set_rules('chats_radio', 'Chats', 'trim|required|in_list[chats]');}
             if (!empty($_POST['enfants_radio'])) {
-                $this->form_validation->set_rules('enfants_radio', 'Enfants', 'trim|required|in_list[enfants]');
-            }
+                $this->form_validation->set_rules('enfants_radio', 'Enfants', 'trim|required|in_list[enfants]');}
             if (!empty($_POST['non_radio'])) {
-                $this->form_validation->set_rules('non_radio', 'Non', 'trim|required|in_list[oui]');
-            }
-            $this->form_validation->set_rules('description_animal', 'Description', 'trim|required');
-
-
-
-            /* ------------------------- */
-            /* Formulaire valide */
-            /* ------------------------- */
+                $this->form_validation->set_rules('non_radio', 'Non', 'trim|required|in_list[oui]');}
+                $this->form_validation->set_rules('description_animal', 'Description', 'trim|required');
 
             if ($this->form_validation->run() == TRUE) {
-                // Recuperation de la valeur des input de $databdd
                 $databdd['nom_animal'] = $this->input->post('nom_animal');
                 $databdd['puce_animal'] = $this->input->post('puce_animal');
                 $databdd['espece_animal'] = $this->input->post('espece_animal');
@@ -60,35 +39,21 @@ class Annonce extends CI_Controller
                 $databdd['sexe_animal'] = $this->input->post('sexe_animal');
                 $databdd['lieu_animal'] = $this->input->post('lieu_animal');
                 $databdd['description_animal'] = $this->input->post('description_animal');
-                // verifie si chiens_radio est vide
                 if (!empty($this->input->post('chiens_radio'))) {
-                    $databdd['chiens_radio'] = $this->input->post('chiens_radio');
-                }
+                    $databdd['chiens_radio'] = $this->input->post('chiens_radio');}
                 if (!empty($this->input->post('chats_radio'))) {
-                    $databdd['chats_radio'] = $this->input->post('chats_radio');
-                }
+                    $databdd['chats_radio'] = $this->input->post('chats_radio');}
                 if (!empty($this->input->post('enfants_radio'))) {
-                    $databdd['enfants_radio'] = $this->input->post('enfants_radio');
-                }
+                    $databdd['enfants_radio'] = $this->input->post('enfants_radio');}
                 if (!empty($this->input->post('non_radio'))) {
-                    $databdd['non_radio'] = $this->input->post('non_radio');
-                }
-                // vérifie si un fichier a été soumis 
+                    $databdd['non_radio'] = $this->input->post('non_radio');}
                 if (!empty($_FILES['image_chat']['name'])) {
-                    // où le fichier téléchargé sera stocké
                     $upload_path = './uploads/annonce/';
-
                     $new_file_name = $this->upload_image_chat('image_chat', $upload_path);
-                    //  vérifie si le téléchargement de l'image a réussi
                     if ($new_file_name) {
-                        //  le nom du fichier est stocké dans la variable $new_file_name
-                        // le fichier est assigné à la clé 'image_chat' dans le tableau:  $databdd
-                        //  l'enregistrement du nom du fichier dans la base de données
                         $databdd['image_chat'] = $new_file_name;
                     }
                 }
-
-                //appel à la méthode create_annonce de la classe "Chat_Model" avec le paramètre du tableau $databdd.
                 $this->Chat_Model->create_annonce($databdd);
                 redirect("Annonce/success");
             } else {
@@ -96,36 +61,26 @@ class Annonce extends CI_Controller
             }
         }
     }
-    // Librairie intégré code igniter 3, upload qui gère le téléchagrement des images
-    // field_name : nom du fichier, upload_path : chemin de destination du fichier
+    
     function upload_image_chat($field_name = '', $upload_path = '')
     {
-        // chemin de destination du fichier, type de fichier, poids maxi, ne se dédouble pas
         $config['upload_path'] = $upload_path;
         $config['allowed_types'] = 'gif|jpg|jpeg|png';
         $config['max_size'] = 30720;
         $config['overwrite'] = true;
-        // on charge la librairie avec la config
         $this->load->library('upload', $config);
-        // On upload l'image dans le dossier upload/annonce
         if ($this->upload->do_upload($field_name)) {
             $upload_data = $this->upload->data();
-
-            // Calculer la somme de hachage SHA-1 du contenu de l'image en 40 caratères aléatoires
             $hash = sha1_file($upload_data['full_path']);
-            // génère un nouveau nom de fichier pour l'image téléchargée haché en .jpg
             $image_nom =  $hash . '.jpg';
-            //  génère le chemin complet du nouveau fichier vers son chemin
             $new_file_path = $config['upload_path'] . $image_nom;
-            // On renomme le fichier téléchargé
             rename($upload_data['full_path'], $new_file_path);
-
             return $image_nom;
         }
     }
+
     public function success()
     {
-        //formulaire true : on refresh vers Users
         header('refresh:10;url=' . base_url('Users'));
         $this->load->view('form/success');
     }
--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -9,11 +9,8 @@ class Users extends CI_Controller
 
     public function index()
     {
-        // on appelle chat_annonce du modèle "User_Model" pour récupérer les informations de l'annonce du chat 
         $chat_annonce = $this->User_Model->get_annonce();
-        // $data affiche les données du chat
         $data['chat_annonce'] = $chat_annonce;
-        // on affiche la vue
         $this->load->view('espace_animaux/recherche', $data);
     }
 
@@ -33,41 +30,26 @@ class Users extends CI_Controller
             redirect('Users');
         } else {
             $this->form_validation->set_rules('email', 'email', 'trim|required', array(
-                'required' => 'L\'email n\'est renseigné'
-            ));
+                'required' => 'L\'email n\'est renseigné'));
             $this->form_validation->set_rules('mdp', 'Mot de passe', 'trim|required', array(
                 'trim' => 'Le mot de passe doit être valide',
-                'required' => 'le mot de passe n\'est renseigné'
-            ));
-
-            //Si la validation du formulaire est true 
+                'required' => 'le mot de passe n\'est renseigné')); 
             if ($this->form_validation->run() == true) {
-                //Si les information de connexion un email + mdp  sont valides
-                // md5 : mdp haché
-                // appele de la méthode cb_users de "User_Model"
                 if ($this->User_Model->cb_users($_POST["email"], md5($_POST["mdp"])) == 1) {
-                    //Authentification réussit, email est extraite de la variable $_POST et stockée dans la variable $email
                     $email = $_POST["email"];
-                    // email dans un tableau
                     $data = array('email' => $email);
-                    // appelle de la méthode get_user_by sur le modèle "User_Model" par rapport au tableau $data 
                     $user = $this->User_Model->get_user_by($data);
-
-                    // Recuperation de la valeur des input
-                    if ($user) {
-                        $session_user = array(
-                            'id' => $user['id'],
-                            'nom' => $user['nom'],
-                            'prenom' => $user['prenom'],
-                            'email' => $email
-                        );
-                    }
-                    // utilisation de  la bibliothèque de sessions intégrée a code igniter
-                    // set_userdata défini les données de $session_user
+                if ($user) {
+                    $session_user = array(
+                        'id' => $user['id'],
+                        'nom' => $user['nom'],
+                        'prenom' => $user['prenom'],
+                        'email' => $email
+                    );
+                }
                     $this->session->set_userdata($session_user);
                     redirect("Users");
                 } else {
-                    // Si les infos de connexion sont false : vue login
                     $data['info_connexion'] = 'error';
                     $this->load->view('espace_user/login', $data);
                 }
@@ -97,40 +79,32 @@ class Users extends CI_Controller
             redirect('Users');
         } else {
             $this->form_validation->set_rules('nom', 'Nom', 'trim|required', array(
-                'required' => 'Le nom doit être renseigné',
-            ));
+                'required' => 'Le nom doit être renseigné',));
             $this->form_validation->set_rules('prenom', 'Prenom', 'trim|required', array(
-                'required' => 'Le prenom doit être renseigné',
-            ));
+                'required' => 'Le prenom doit être renseigné',));
             $this->form_validation->set_rules('pseudo', 'Pseudo', 'trim|required|is_unique[users.pseudo]', array(
                 'required' => 'Le pseudo doit être valide',
-                'is_unique' => 'Le pseudo existe déjà'
-            ));
+                'is_unique' => 'Le pseudo existe déjà'));
             $this->form_validation->set_rules('mdp', 'Mot de passe', 'trim|required', array(
                 'trim' => 'Le mot de passe doit être valide',
-                'required' => 'Le mot de passe n\'est pas renseigné'
-            ));
+                'required' => 'Le mot de passe n\'est pas renseigné'));
             $this->form_validation->set_rules('mdp_confirm', 'Confirmation du mot de passe', 'trim|required|matches[mdp]', array(
                 'trim' => 'Le mot de passe doit être valide',
                 'required' => 'Le mot de passe n\'est renseigné',
-                'matches' => 'Le mot de passe n\'est pas le même'
-            ));
+                'matches' => 'Le mot de passe n\'est pas le même'));
             $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[users.email]', array(
                 'valid_email' => 'L\'email doit être valide',
-                'is_unique' => 'L\'email existe déjà'
-            ));
-
+                'is_unique' => 'L\'email existe déjà'));
+                
             if ($this->form_validation->run() == FALSE) {
                 $this->load->view('espace_user/inscription');
             } else {
-                // Recuperation de la valeur des input
                 $nom = $this->input->post('nom');
                 $prenom = $this->input->post('prenom');
                 $pseudo = $this->input->post('pseudo');
                 $mdp = md5($this->input->post('mdp'));
                 $email = ($this->input->post('email'));
 
-                //$data stock dans un tableau les variables et les valeurs
                 $data = array(
                     'nom' => $nom,
                     'prenom' => $prenom,
@@ -138,10 +112,7 @@ class Users extends CI_Controller
                     'mdp' => $mdp,
                     'email' => $email
                 );
-                //appel à la méthode create_user de la classe "User_Model" avec les données contenues dans le tableau $data. 
                 $this->User_Model->create_user($data);
-
-                // la pop up = success_message + elle est chargé sur la vue inscription
                 $data['popup'] = true;
                 $data['success_message'] = 'Vous êtes bien inscrit, vous pouvez dès maintenant vous connecter !';
                 $this->load->view('espace_user/inscription', $data);
@@ -149,37 +120,22 @@ class Users extends CI_Controller
         }
     }
 
-    /* ---------------------------------------------------- */
-    /* Envoi email pour recupérer le mdp */
-    /* ---------------------------------------------------- */
     public function mail()
     {
         $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email', array(
             'valid_email' => 'L\'adresse mail doit être valide'
         ));
-        // Recuperation de la valeur des input
         $email = $this->input->post('email');
-
         if ($this->form_validation->run() == FALSE) {
             $this->load->view('espace_user/mail');
         } else {
-            // verifier si l'email existe dans la bdd
             if ($this->User_Model->exist_email($email)) {
-                // ligne génèré 30 octets de données aléatoires en utilisant la fonction random_bytes de PHP
                 $number = bin2hex(random_bytes(30));
-                // appel à la méthode new_number de la classe "User_Model"
-                // enregistrer le numéro généré ($number) dans la base de données
                 $this->User_Model->new_number($number, $email);
-
-                // charge la config pour l'envoi d'email
                 $this->load->config('email');
-                // smtp :  transfert d'e-mails entre les serveurs de messagerie électronique.
                 $from = $this->config->item('smtp_user');
-
-                //lien vers la page de recup_mdp avec la clé généré
                 $link = anchor('/Users/mdp_recup/'  . $number, 'Reinitialiser votre mot de passe');
 
-                //Contenu du mail une fois envoyé
                 $this->email->from($from, 'Adopt Kitty');
                 $this->email->to($email);
                 $this->email->subject('Mot de passe oublié');
@@ -192,9 +148,6 @@ class Users extends CI_Controller
                 $message .= '</body></html>';
                 $this->email->message($message);
 
-
-
-                //Pop up affiché une fois que l'email a été envoyé : true 
                 if ($this->email->send()) {
                     $popup = true;
                     $this->load->view('espace_user/mail', compact('popup'));
@@ -215,7 +168,6 @@ class Users extends CI_Controller
 
     public function mdp_recup($mdp_recup = '')
     {
-        // chargé la méthode number_exist sur User_Model sur le paramètre $mdp_recup
         if ($this->User_Model->number_exist($mdp_recup)) {
             $this->form_validation->set_rules('mdp', 'mdp', 'trim|required');
             $this->form_validation->set_rules('mdp_confirm', 'Confirmation du mot de passe', 'trim|required|matches[mdp]');
@@ -224,15 +176,12 @@ class Users extends CI_Controller
                 $this->load->view('espace_user/mdp_recup');
             } else {
                 $mdp = md5($this->input->post('mdp'));
-                // chargé la méthode update_mdp sur User_Model sur le paramètre $mdp et $mdp_recup
                 $this->User_Model->update_mdp($mdp, $mdp_recup);
-
                 $data['popup'] = true;
                 $data['success_message'] = 'Vous avez bien enregistré votre nouveau mot de passe. Vous pouvez dès maintenant vous connecter !';
                 $this->load->view('espace_user/mdp_recup', $data);
             }
         } else {
-            // Si le lien n'est plus valide + la clé :
             header('refresh:5;url=' . base_url('Users'));
             echo 'La clé de récupération n\'est pas valide';
         }
