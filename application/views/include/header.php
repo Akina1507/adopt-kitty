f2563ea60e6e05e132dbe5ab679e8e572c460227
Akina <mswietrich1507@gmail.com>
2024-02-07 16:59:53 +0100

    🚧New NavBar✨

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -49,3 +49,45 @@
         </div>
     </div>
     </div>
+
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+  <img src="./assets/img/adopt-kitty-logo.png" alt="Logo" width="100" height="100" class="d-inline-block align-text-top">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Annonces
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Voir les Annonces</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Famille d'accueil</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Informations
+          </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Nous contacter</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">FAQ</a></li>
+            </ul>
+        </li>
+      </ul> 
+      <ul class="navbar-nav">
+      <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Connexion / Inscription</a>
+        </li>
+    </ul>
+    </div>
+  </div>
+</nav>
