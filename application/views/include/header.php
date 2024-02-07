2decdefcf76e41852c81f21a9a3cefd9b457e247
Akina <mswietrich1507@gmail.com>
2024-02-07 14:54:39 +0100

    🐛 Bug CSS 🐛

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/adopt-kitty-1/assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
     <title>Adopt'Kitty</title>
 </head>
 
