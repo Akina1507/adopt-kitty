a66defaae2898372f46b43a032a454155b2a7353
Akina <mswietrich1507@gmail.com>
2024-02-07 14:57:41 +0100

    🐛 Bug Logo 🐛

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./assets/css/style.css">
     <title>Adopt'Kitty</title>
@@ -14,7 +14,7 @@
 <body>
     <div class="container">
         <div class="col d-flex justify-content-center align-items-center ">
-            <a href="<?= base_url("Users") ?>"><img class="logo" src="/adopt-kitty-1/assets/img/adopt-kitty-logo" width="200" height="180"></a>
+            <a href="<?= base_url("Users") ?>"><img class="logo" src="./assets/img/adopt-kitty-logo" width="200" height="180"></a>
             <ul class="nav nav-tabs">
                 <li class="nav-item">
                     <a class="nav-link" href="<?= base_url("Users") ?>">Adopter un animal</a>
