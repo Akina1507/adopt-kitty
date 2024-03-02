8f12167a50c14f6cde65f7a4caebf4b1763c0260
Akina <mswietrich1507@gmail.com>
2024-03-02 17:56:37 +0100

    🚧 Footer 🚧

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,3 +1,36 @@
+<footer class="bg-body-tertiary text-center">
+  <div class="container p-4 pb-0">
+    <section class="mb-4">
+      <!-- Linkedin -->
+      <a
+        data-mdb-ripple-init
+        class="btn text-white btn-floating m-1"
+        style="background-color: #0082ca;"
+        href="https://www.linkedin.com/in/marie-sarah-wietrich-747b3a23b/"
+        role="button">
+        <ion-icon name="logo-linkedin"></ion-icon>
+      </a>
+      <!-- Github -->
+      <a
+        data-mdb-ripple-init
+        class="btn text-white btn-floating m-1"
+        style="background-color: #333333;"
+        href="https://github.com/Akina1507"
+        role="button">
+        <ion-icon name="logo-github"></ion-icon>
+      </a>
+    </section>
+  </div>
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2024 Copyright - Adopt Kitty
+  </div>
+  <!-- Copyright -->
+</footer>
+
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 <script src="<?= base_url('assets/js/script.js')?>"></script>
