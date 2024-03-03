d36cc8a31eebd2231083cb863fcbefe2936c96e7
Akina <mswietrich1507@gmail.com>
2024-03-03 18:22:46 +0100

    🚧 Contact + Connexion 🚧

--- application/views/espace_user/login.php
+++ application/views/espace_user/login.php
@@ -8,8 +8,33 @@
             <div class="col-md-4 p-5 container-login bg-white bordered">
 
 
-                <h4 class="text-center">Connexion</h4>
-                <h6 class="text-center">Utilisateurs</h6>
+            <ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+  <li class="nav-item" role="presentation">
+    <a
+      class="nav-link active"
+      id="tab-login"
+      data-mdb-pill-init
+      href="#pills-login"
+      role="tab"
+      aria-controls="pills-login"
+      aria-selected="true"
+      >Login</a
+    >
+  </li>
+  <li class="nav-item" role="presentation">
+    <a
+      class="nav-link"
+      id="tab-register"
+      data-mdb-pill-init
+      href="#pills-register"
+      role="tab"
+      aria-controls="pills-register"
+      aria-selected="false"
+      >Register</a
+    >
+  </li>
+</ul>
+<!-- Pills navs -->
                 <div class="form-group">
 
                     <div class="field">
@@ -27,14 +52,16 @@
                 <br>
                 <div class="row justify-content-between">
                     <div class="col d-flex justify-content-between">
-                        <a class="btn btn-outline-dark fw-bold m-1" href="<?= base_url("Users/accueil") ?>">Retour</a>
-                        <input class="btn btn-outline-dark fw-bold m-1" type="submit" name="envoyer">
+                        <input class="btn btn-primary btn-block mb-4" type="submit" name="envoyer">
                     </div>
                 </div>
                 <br>
                 <div class="col d-flex justify-content-between text-center">
+                  <div class="form-check mb-3 mb-md-0">
+                    <input class="form-check-input" type="checkbox" value="" id="loginCheck" checked/>
+                    <label class="form-check-label" for="loginCheck"> Remember me </label>
+                  </div>
                     <a href="<?= base_url("Users/mail") ?>">Mot de passe oublié ?</a>
-                    <a href="<?= base_url("Users/inscription") ?>">Créer un compte</a>
                 </div>
                 <?php echo form_close(); ?>
                 <?= form_error('formulaire'); ?>
@@ -43,3 +70,186 @@
     </div>
 </div>
 </div>
+
+
+<!-- Pills navs -->
+<ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+  <li class="nav-item" role="presentation">
+    <a
+      class="nav-link active"
+      id="tab-login"
+      data-mdb-pill-init
+      href="#pills-login"
+      role="tab"
+      aria-controls="pills-login"
+      aria-selected="true"
+      >Login</a
+    >
+  </li>
+  <li class="nav-item" role="presentation">
+    <a
+      class="nav-link"
+      id="tab-register"
+      data-mdb-pill-init
+      href="#pills-register"
+      role="tab"
+      aria-controls="pills-register"
+      aria-selected="false"
+      >Register</a
+    >
+  </li>
+</ul>
+<!-- Pills navs -->
+
+<!-- Pills content -->
+<div class="tab-content">
+  <div
+    class="tab-pane fade show active"
+    id="pills-login"
+    role="tabpanel"
+    aria-labelledby="tab-login"
+  >
+    <form>
+      <div class="text-center mb-3">
+        <p>Sign in with:</p>
+        <button data-mdb-ripple-init type="button" class="btn btn-secondary btn-floating mx-1">
+          <i class="fab fa-facebook-f"></i>
+        </button>
+
+        <button data-mdb-ripple-init type="button" class="btn btn-secondary btn-floating mx-1">
+          <i class="fab fa-google"></i>
+        </button>
+
+        <button data-mdb-ripple-init type="button" class="btn btn-secondary btn-floating mx-1">
+          <i class="fab fa-twitter"></i>
+        </button>
+
+        <button data-mdb-ripple-init type="button" class="btn btn-secondary btn-floating mx-1">
+          <i class="fab fa-github"></i>
+        </button>
+      </div>
+
+      <p class="text-center">or:</p>
+
+      <!-- Email input -->
+      <div data-mdb-input-init class="form-outline mb-4">
+        <input type="email" id="loginName" class="form-control" />
+        <label class="form-label" for="loginName">Email or username</label>
+      </div>
+
+      <!-- Password input -->
+      <div data-mdb-input-init class="form-outline mb-4">
+        <input type="password" id="loginPassword" class="form-control" />
+        <label class="form-label" for="loginPassword">Password</label>
+      </div>
+
+      <!-- 2 column grid layout -->
+      <div class="row mb-4">
+        <div class="col-md-6 d-flex justify-content-center">
+          <!-- Checkbox -->
+          <div class="form-check mb-3 mb-md-0">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              value=""
+              id="loginCheck"
+              checked
+            />
+            <label class="form-check-label" for="loginCheck"> Remember me </label>
+          </div>
+        </div>
+
+        <div class="col-md-6 d-flex justify-content-center">
+          <!-- Simple link -->
+          <a href="#!">Forgot password?</a>
+        </div>
+      </div>
+
+      <!-- Submit button -->
+      <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
+
+      <!-- Register buttons -->
+      <div class="text-center">
+        <p>Not a member? <a href="#!">Register</a></p>
+      </div>
+    </form>
+  </div>
+  <div
+    class="tab-pane fade"
+    id="pills-register"
+    role="tabpanel"
+    aria-labelledby="tab-register"
+  >
+    <form>
+      <div class="text-center mb-3">
+        <p>Sign up with:</p>
+        <button data-mdb-ripple-init type="button" class="btn btn-secondary btn-floating mx-1">
+          <i class="fab fa-facebook-f"></i>
+        </button>
+
+        <button data-mdb-ripple-init type="button" class="btn btn-secondary btn-floating mx-1">
+          <i class="fab fa-google"></i>
+        </button>
+
+        <button data-mdb-ripple-init type="button" class="btn btn-secondary btn-floating mx-1">
+          <i class="fab fa-twitter"></i>
+        </button>
+
+        <button data-mdb-ripple-init type="button" class="btn btn-secondary btn-floating mx-1">
+          <i class="fab fa-github"></i>
+        </button>
+      </div>
+
+      <p class="text-center">or:</p>
+
+      <!-- Name input -->
+      <div data-mdb-input-init class="form-outline mb-4">
+        <input type="text" id="registerName" class="form-control" />
+        <label class="form-label" for="registerName">Name</label>
+      </div>
+
+      <!-- Username input -->
+      <div data-mdb-input-init class="form-outline mb-4">
+        <input type="text" id="registerUsername" class="form-control" />
+        <label class="form-label" for="registerUsername">Username</label>
+      </div>
+
+      <!-- Email input -->
+      <div data-mdb-input-init class="form-outline mb-4">
+        <input type="email" id="registerEmail" class="form-control" />
+        <label class="form-label" for="registerEmail">Email</label>
+      </div>
+
+      <!-- Password input -->
+      <div data-mdb-input-init class="form-outline mb-4">
+        <input type="password" id="registerPassword" class="form-control" />
+        <label class="form-label" for="registerPassword">Password</label>
+      </div>
+
+      <!-- Repeat Password input -->
+      <div data-mdb-input-init class="form-outline mb-4">
+        <input type="password" id="registerRepeatPassword" class="form-control" />
+        <label class="form-label" for="registerRepeatPassword">Repeat password</label>
+      </div>
+
+      <!-- Checkbox -->
+      <div class="form-check d-flex justify-content-center mb-4">
+        <input
+          class="form-check-input me-2"
+          type="checkbox"
+          value=""
+          id="registerCheck"
+          checked
+          aria-describedby="registerCheckHelpText"
+        />
+        <label class="form-check-label" for="registerCheck">
+          I have read and agree to the terms
+        </label>
+      </div>
+
+      <!-- Submit button -->
+      <button data-mdb-ripple-init type="submit" class="btn btn-primary btn-block mb-3">Sign in</button>
+    </form>
+  </div>
+</div>
+<!-- Pills content -->
--- application/views/form/contact.php
+++ application/views/form/contact.php
@@ -1,18 +1,37 @@
 <form>
-<div class="col-md-6 mb-3">
-    <label for="nom_user">Nom :</label>
-    <input class="form-control" type="text" name="nom_user" id="nom_user" placeholder="Votre nom" required>
+    <h2>Contactez nous</h2>
+<div class="row mb-4">
+    <div class="col">
+      <div data-mdb-input-init class="form-outline">
+        <label for="nom_user">Nom :</label>
+        <input class="form-control" type="text" name="nom_user" id="nom_user" placeholder="Votre nom" required>
         <h6 class="invalid-from-error"><?= form_error('nom_user'); ?></h6>
-</div>
-<div class="col-md-6 mb-3">
-    <label for="prenom_user">Prénom :</label>
-    <input class="form-control" type="text" name="prenom_user" id="prenom_user" placeholder="Votre prénom" required>
+      </div>
+    </div>
+    <div class="col">
+      <div data-mdb-input-init class="form-outline">
+        <label for="prenom_user">Prénom :</label>
+        <input class="form-control" type="text" name="prenom_user" id="prenom_user" placeholder="Votre prénom" required>
         <h6 class="invalid-from-error"><?= form_error('prenom_user'); ?></h6>
+      </div>
+    </div>
+  </div>
+
+
+<div class="col-mb-4">
+    <label for="nom_entreprise">Nom entreprise :</label>
+    <input class="form-control" type="text" name="nom_entreprise" id="nom_entreprise">
+        <h6 class="invalid-from-error"><?= form_error('nom_entreprise'); ?></h6>
 </div>
-<div class="col-md-6 mb-3">
+<div class="col-mb-4">
     <label for="email_user">Email :</label>
     <input class="form-control" type="email" name="email_user" id="email_user" placeholder="votreadresse@email.com" required>
         <h6 class="invalid-from-error"><?= form_error('email_user'); ?></h6>
+</div>
+<div class="col-mb-4">
+    <label for="phone_user">Téléphone :</label>
+    <input class="form-control" type="number" name="phone_user" id="phone_user" placeholder="+33" required>
+        <h6 class="invalid-from-error"><?= form_error('phone_user'); ?></h6>
 </div>
 <div class="col-md-12 mb-3">
     <label for="message_contact">Votre message</label>
